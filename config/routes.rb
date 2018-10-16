f2b4795a84c90e3e44439de7ddfc54537a5aa77a
Hakxel <asiel_montes@yahoo.com>
2018-10-16 16:15:11 -0400

    Add header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'static_pages#home'
-  get 'static_pages/home' => 'static_pages#home'
-  get 'static_pages/about' => 'static_pages#about'
+
+  get '/about' => 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
