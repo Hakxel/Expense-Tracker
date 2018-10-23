82a8a36dc757dedbffcd4742361193781a844408
Hakxel <asiel_montes@yahoo.com>
2018-10-23 19:23:35 -0400

    Create initial sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  # get '/', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
+  resources :users
+
 end
