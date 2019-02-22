b5dabc077cc8c97b16ce994bc79fa5863f7c1f20
Hakxel <asiel_montes@yahoo.com>
2019-02-22 15:25:47 -0500

    Add show to users controller before actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:edit, :update]
-before_action :correct_user, only: [:edit, :update]
+before_action :logged_in_user, only: [:show, :edit, :update]
+before_action :correct_user, only: [:show, :edit, :update]
 
   def index
   end
 
-  def show
-    @user = User.find(params[:id])
+  def show    
+    @user = User.find(params[:id])  
   end
 
   def new
@@ -59,7 +59,7 @@ before_action :correct_user, only: [:edit, :update]
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to(users_path) unless current_user?(@user)
     end
 
 
